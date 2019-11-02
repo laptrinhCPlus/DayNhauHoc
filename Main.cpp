5edf2b05d24b1b0cf40b611e73ceeb5081f3e564
Nguyen Quoc Dat <boelovenhan@gmail.com>
2019-11-02 17:58:23 +0700

    Example 2 Nov 2019

---
+++ Main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+// Sum calculator : 1/ (1*2) +.....+ 1/(n*(n+1))
+float SumExample6(int nValue);
+// Sum calculator : 1/2 +.....+ n/((n+1))
+float SumExample7(int nValue);
+// Sum calculator : 1/ 2 +.....+ (2*n +1)/(2*n +2)
+float SumExample8(int nValue);
+// Sum calculator : 1*2*3 ....*n
+float SumExample9(int nValue);
+
+//Main
+int main(){
+  int nValue = 0;
+  cout<<"Nhap gia tri N : ";
+  cin>>nValue;
+  // Call the function to Sum calculator at here
+
+  system("pause");
+  return 0;
+}
+
+// function
+
+float SumExample6(int nValue){
+  if(nValue == 0){
+    return 0;
+  }
+  else{
+    return ((float)1/(nValue * (nValue+1))) + SumExample6(nValue-1);
+  }
+}
+
+float SumExample7(int nValue){
+  if(nValue == 0){
+    return 0;
+  }
+  else{
+    return ((float)nValue/(nValue+1))+ SumExample7(nValue-1);
+  }
+}
+
+float SumExample8(int nValue){
+  if(nValue == 0){
+    return 0;
+  }
+  else{
+    return ((float)(2*nValue +1 ))/(2*nValue +2))+ TinhTongBai8(nValue-1);
+  }
+}
+
+float SumExample9(int nValue){
+  if(nValue == 1){
+    return 1;
+  }else{
+    return nValue * SumExample9(nValue - 1);
+  }
+}
